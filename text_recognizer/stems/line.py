7bccfc9f291a395f60204ca1f5daab4b2f1b16e0
Niall Turbitt <niall.turbitt@databricks.com>
2023-01-03 17:11:07 +0000

    refactor out closure due to pickling issue

--- text_recognizer/stems/line.py
+++ text_recognizer/stems/line.py
@@ -3,7 +3,6 @@ import random
 from PIL import Image
 from torchvision import transforms
 
-
 import text_recognizer.metadata.iam_lines as metadata
 from text_recognizer.stems.image import ImageStem
 
@@ -40,27 +39,7 @@ class IAMLineStem(ImageStem):
     def __init__(self, augment=False, color_jitter_kwargs=None, random_affine_kwargs=None):
         super().__init__()
 
-        def embed_crop(crop, augment=augment):
-            # crop is PIL.image of dtype="L" (so values range from 0 -> 255)
-            image = Image.new("L", (metadata.IMAGE_WIDTH, metadata.IMAGE_HEIGHT))
-
-            # Resize crop
-            crop_width, crop_height = crop.size
-            new_crop_height = metadata.IMAGE_HEIGHT
-            new_crop_width = int(new_crop_height * (crop_width / crop_height))
-            if augment:
-                # Add random stretching
-                new_crop_width = int(new_crop_width * random.uniform(0.9, 1.1))
-                new_crop_width = min(new_crop_width, metadata.IMAGE_WIDTH)
-            crop_resized = crop.resize((new_crop_width, new_crop_height), resample=Image.BILINEAR)
-
-            # Embed in the image
-            x = min(metadata.CHAR_WIDTH, metadata.IMAGE_WIDTH - new_crop_width)
-            y = metadata.IMAGE_HEIGHT - new_crop_height
-
-            image.paste(crop_resized, (x, y))
-
-            return image
+        self.augment = augment
 
         if color_jitter_kwargs is None:
             color_jitter_kwargs = {"brightness": (0.8, 1.6)}
@@ -72,10 +51,32 @@ class IAMLineStem(ImageStem):
                 "fill": 0,
             }
 
-        pil_transforms_list = [transforms.Lambda(embed_crop)]
+        pil_transforms_list = [transforms.Lambda(self.embed_crop)]
         if augment:
             pil_transforms_list += [
                 transforms.ColorJitter(**color_jitter_kwargs),
                 transforms.RandomAffine(**random_affine_kwargs),
             ]
         self.pil_transforms = transforms.Compose(pil_transforms_list)
+
+    def embed_crop(self, crop):
+        # crop is PIL.image of dtype="L" (so values range from 0 -> 255)
+        image = Image.new("L", (metadata.IMAGE_WIDTH, metadata.IMAGE_HEIGHT))
+
+        # Resize crop
+        crop_width, crop_height = crop.size
+        new_crop_height = metadata.IMAGE_HEIGHT
+        new_crop_width = int(new_crop_height * (crop_width / crop_height))
+        if self.augment:
+            # Add random stretching
+            new_crop_width = int(new_crop_width * random.uniform(0.9, 1.1))
+            new_crop_width = min(new_crop_width, metadata.IMAGE_WIDTH)
+        crop_resized = crop.resize((new_crop_width, new_crop_height), resample=Image.BILINEAR)
+
+        # Embed in the image
+        x = min(metadata.CHAR_WIDTH, metadata.IMAGE_WIDTH - new_crop_width)
+        y = metadata.IMAGE_HEIGHT - new_crop_height
+
+        image.paste(crop_resized, (x, y))
+
+        return image
