1cd7ee7123bf4d2525039f0a18ef2a488c4e5970
Niall Turbitt <niall.turbitt@databricks.com>
2023-01-04 11:06:00 +0000

    update

--- text_recognizer/paragraph_text_recognizer.py
+++ text_recognizer/paragraph_text_recognizer.py
@@ -4,9 +4,6 @@ Example usage as a script:
 
   python text_recognizer/paragraph_text_recognizer.py \
     text_recognizer/tests/support/paragraphs/a01-077.png
-
-  python text_recognizer/paragraph_text_recognizer.py \
-    https://fsdl-public-assets.s3-us-west-2.amazonaws.com/paragraphs/a01-077.png
 """
 import argparse
 from pathlib import Path
